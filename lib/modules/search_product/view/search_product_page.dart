267c8ce4c63b7755978c09396db69656eb0ce03c
nabil <nabilnazmul868@gmail.com>
2022-05-27 17:49:46 +0600

    search product page modified
    custom loader added and breaking down the search product page view into smaller widgets to reduce build cost...

--- lib/modules/search_product/view/search_product_page.dart
+++ lib/modules/search_product/view/search_product_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_polygon/flutter_polygon.dart';
 
 import '../../../core/app_colors.dart';
+import '../widgets/custom_textfield.dart';
 
 class SearchProduct extends StatelessWidget {
   const SearchProduct({Key? key}) : super(key: key);
@@ -19,33 +20,7 @@ class SearchProduct extends StatelessWidget {
 
              SizedBox(height: 50,),
             //Textfield
-            Padding(
-              padding: const EdgeInsets.only(left: 15,right: 15,bottom: 32),
-              child: SizedBox(
-                height: 48,
-                child: TextField(
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        onPressed:() {
-
-                        },
-                        icon: const Icon(Icons.search,
-                          color: AppColors.textfieldSearchIconColor,),
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      filled: true,
-                      hintStyle: const TextStyle(color: AppColors.hintTextColor,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 12),
-                      hintText: "কাঙ্ক্ষিত পণ্যটি খুঁজুন",
-                      fillColor: AppColors.colorWhite),
-
-                ),
-              ),
-            ),
+            CustomTextField(),
 
             Expanded(
               child: Padding(
